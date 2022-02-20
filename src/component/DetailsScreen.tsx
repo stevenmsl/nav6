78b1a394ae502c20192ee1aac8ac0763fb1e0c16
Steven Lin <steven_lx@outlook.com>
2022-02-19 17:26:48 -0800

    screens navigation

--- src/component/DetailsScreen.tsx
+++ src/component/DetailsScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../navigation/types';
 
-const DetailsScreen = () => {
+interface DetailsScreenProps
+  extends NativeStackScreenProps<RootStackParamList, 'Details'> {}
+const DetailsScreen: React.FC<DetailsScreenProps> = ({navigation}) => {
   return (
     <View style={styles.Screen}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to the first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 };
